efb29cde6fd94d9d3c9f60e571f5f8cf5e13ff3f
imranshaheer <is23acc@herts.ac.uk>
2024-11-22 00:46:11 +0000

    commit

--- code.r
+++ code.r
@@ -1,2 +1,7 @@
 install.packages("dplyr")
 install.packages("ggplot2")
+# Load required libraries
+library(dplyr)
+# 1. Load the datasets
+attendance <- read.csv("DataSet/secondary-attendance-csv-2.csv")
+free_school_meals <- read.csv("DataSet/secondary-free-school-meals-csv-1.csv")
